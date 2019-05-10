287f1a1d36514c8b666eb38a0cdbd53920e1bbd8
Dax Wann <daxwann@gmail.com>
2019-05-10 15:39:44 -0700

    add form partial

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -6,15 +6,41 @@ class CatsController < ApplicationController
 
   def show
     @kitty = Cat.find_by(id: params[:id])
-    render :show 
+
+    if @kitty
+      render :show 
+    else
+      redirect_to books_url
+    end
   end
 
   def new
+    @kitty = Cat.new
     render :new
+  end
+  
+  def create
+    @kitty = Cat.new(cat_params)
+    
+    if @kitty.save
+      redirect_to cat_url(@kitty)
+    else
+      render :new
+    end
   end
 
   def edit
     @kitty = Cat.find_by(id: params[:id])
     render :edit
+  end
+
+  def update
+    @kitty = Cat.find_by(id: params[:id])
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :sex, :color, :description, :birth_date)
   end
 end
